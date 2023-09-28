e6f1bc63d11908cf5df88fd6b59b9cf9f5ab4ef9
Alejandro11178 <aleaafaros@gmail.com>
2023-09-27 21:58:56 -0300

    Ultimo avance

--- misitio/core/views.py
+++ misitio/core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import logout,authenticate, login as auth_login  
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def iniciar_sesion (request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)  
+            return redirect('/admin/')
+        else:
+            messages.error(request, "Nombre o contraseña incorrectos, ingrese nuevamente")
+
+    return render(request, 'core/login.html')
+
